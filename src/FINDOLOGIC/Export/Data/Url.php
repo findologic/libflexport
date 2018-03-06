2ecc3c766ec3258a45b3502d28b7fc146f2f05e4
Markus Machatschek <mmachatschek@yahoo.com>
2018-03-06 21:11:36 +0100

    Remove obsolet UsergroupAwareUrlValue class

--- src/FINDOLOGIC/Export/Data/Url.php
+++ src/FINDOLOGIC/Export/Data/Url.php
@@ -2,12 +2,22 @@
 
 namespace FINDOLOGIC\Export\Data;
 
-use FINDOLOGIC\Export\Helpers\UsergroupAwareUrlValue;
+use FINDOLOGIC\Export\Helpers\DataHelper;
+use FINDOLOGIC\Export\Helpers\UsergroupAwareSimpleValue;
 
-class Url extends UsergroupAwareUrlValue
+class Url extends UsergroupAwareSimpleValue
 {
     public function __construct()
     {
         parent::__construct('urls', 'url');
+    }
+
+    protected function validate($value)
+    {
+        $value = parent::validate($value);
+
+        $value = DataHelper::validateUrl($value);
+
+        return $value;
     }
 }
