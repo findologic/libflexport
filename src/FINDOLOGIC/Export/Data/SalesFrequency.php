50b1272ae4163ea5b581954aeee20395558f9fe6
Markus Machatschek <mmachatschek@yahoo.com>
2018-03-30 15:23:10 +0200

    Check if salesfrequency is a non negative integer

--- src/FINDOLOGIC/Export/Data/SalesFrequency.php
+++ src/FINDOLOGIC/Export/Data/SalesFrequency.php
@@ -2,12 +2,34 @@
 
 namespace FINDOLOGIC\Export\Data;
 
-use FINDOLOGIC\Export\Helpers\UsergroupAwareNumericValue;
+use FINDOLOGIC\Export\Helpers\UsergroupAwareSimpleValue;
+use FINDOLOGIC\Export\Helpers\EmptyValueNotAllowedException;
 
-class SalesFrequency extends UsergroupAwareNumericValue
+class ValueIsNotPositivIntegerException extends \RuntimeException
+{
+    public function __construct()
+    {
+        parent::__construct('Value is not a positiv integer!');
+    }
+}
+
+class SalesFrequency extends UsergroupAwareSimpleValue
 {
     public function __construct()
     {
         parent::__construct('salesFrequencies', 'salesFrequency');
+    }
+
+    protected function validate($value)
+    {
+        if ($value === '') {
+            throw new EmptyValueNotAllowedException();
+        }
+
+        if (!is_int($value) || $value < 0) {
+            throw new ValueIsNotPositivIntegerException();
+        }
+
+        return $value;
     }
 }
