6f5eb168b4017243702db1031135f265e97cc154
Markus Machatschek <mmachatschek@users.noreply.github.com>
2018-04-29 17:46:07 +0200

    Check if sort value is integer (#52)

--- src/FINDOLOGIC/Export/Data/Sort.php
+++ src/FINDOLOGIC/Export/Data/Sort.php
@@ -2,12 +2,34 @@
 
 namespace FINDOLOGIC\Export\Data;
 
-use FINDOLOGIC\Export\Helpers\UsergroupAwareNumericValue;
+use FINDOLOGIC\Export\Helpers\UsergroupAwareSimpleValue;
+use FINDOLOGIC\Export\Helpers\EmptyValueNotAllowedException;
 
-class Sort extends UsergroupAwareNumericValue
+class ValueIsNotIntegerException extends \RuntimeException
+{
+    public function __construct($value)
+    {
+        parent::__construct(sprintf('%s is not an integer!', $value));
+    }
+}
+
+class Sort extends UsergroupAwareSimpleValue
 {
     public function __construct()
     {
         parent::__construct('sorts', 'sort');
+    }
+
+    protected function validate($value)
+    {
+        if ($value === '') {
+            throw new EmptyValueNotAllowedException();
+        }
+
+        if (!is_int($value)) {
+            throw new ValueIsNotIntegerException($value);
+        }
+
+        return $value;
     }
 }
