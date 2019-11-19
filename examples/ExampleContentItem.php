9cc5c3e35430516e2736745af0451cc019d55ebf
Tobias Graml <51320851+TobiasGraml11@users.noreply.github.com>
2019-11-19 09:45:04 +0100

    Extend usage examples (#112)

--- examples/ExampleContentItem.php
+++ examples/ExampleContentItem.php
@@ -2,82 +2,83 @@
 
 use FINDOLOGIC\Export\Data\Image;
 
-class ItemContent
+/*
+ * This class represents an example content.
+ * As this is just a static class, an own logic must be implemented
+ */
+class ExampleContentItem extends ExampleBaseItem
 {
-    const defaultUserGroup = '';
-    const specificUserGroup = 'LNrLF7BRVJ0toQ==';
-    
     public $id = 'content_ypy44hn5rpk8nggba8vxmpx68d8v7ra';
 
     public $orderNumbers = [
-        self::defaultUserGroup => [
+        self::DEFAULT_USER_GROUP => [
             'pdf304xyz',
             '9424585418519'
         ],
-        self::specificUserGroup => [
+        self::SPECIFIC_USER_GROUP => [
             'pdf305xyz'
         ]
     ];
 
     public $names = [
-        self::defaultUserGroup => 'Aliquam eget vehicula.'
+        self::DEFAULT_USER_GROUP => 'Aliquam eget vehicula.'
     ];
 
     public $summaries = [
-        self::defaultUserGroup => 'Nullam blandit in ipsum ac feugiat. Vivamus varius, velit nec.'
+        self::DEFAULT_USER_GROUP => 'Nullam blandit in ipsum ac feugiat. Vivamus varius, velit nec.'
     ];
 
     public $descriptions = [
-        self::defaultUserGroup =>
+        self::DEFAULT_USER_GROUP =>
             'In tempus eleifend orci, eu suscipit dolor pellentesque ac. Morbi.'
     ];
 
     public $prices = [
-        self::defaultUserGroup => 0
+        self::DEFAULT_USER_GROUP => 0
     ];
 
     public $urls = [
-        self::defaultUserGroup => 'https://www.store.com/documents/pdf304xyz.pdf',
-        self::specificUserGroup => 'https://www.store.com/documents/pdf305xyz.pdf',
+        self::DEFAULT_USER_GROUP => 'https://www.store.com/documents/pdf304xyz.pdf',
+        self::SPECIFIC_USER_GROUP => 'https://www.store.com/documents/pdf305xyz.pdf',
     ];
 
     public $keywords = [
-        self::defaultUserGroup => [
+        self::DEFAULT_USER_GROUP => [
             'pdf304xyz',
             '9424585418519'
         ],
-        self::specificUserGroup => [
+        self::SPECIFIC_USER_GROUP => [
             'pdf305xyz'
         ]
     ];
 
     public $bonuses = [
-        self::defaultUserGroup => 7
+        self::DEFAULT_USER_GROUP => 7
     ];
 
     public $salesFrequencies = [
-        self::defaultUserGroup => 23
+        self::DEFAULT_USER_GROUP => 23
     ];
 
     public $dateAddeds = [
-        self::defaultUserGroup => '2019-10-31T10:20:28+02:00'
+        self::DEFAULT_USER_GROUP => '2019-10-31T10:20:28+02:00'
     ];
 
     public $sorts = [
-        self::defaultUserGroup => 1
+        self::DEFAULT_USER_GROUP => 1
     ];
 
     public $userGroups = [
-        self::specificUserGroup,
+        self::SPECIFIC_USER_GROUP,
         'cHBw'
     ];
 
     public $images = [
-        self::defaultUserGroup => [
+        self::DEFAULT_USER_GROUP => [
             'https://www.store.com/images/pdf304xyz.png' => Image::TYPE_DEFAULT,
             'https://www.store.com/images/thumbnails/pdf304xyz.png' => Image::TYPE_THUMBNAIL
         ],
-        self::specificUserGroup => [
+        self::SPECIFIC_USER_GROUP => [
             'https://www.store.com/images/pdf305xyz.png' => Image::TYPE_DEFAULT,
             'https://www.store.com/images/thumbnails/pdf305xyz.png' => Image::TYPE_THUMBNAIL
         ]
@@ -91,10 +92,10 @@ class ItemContent
 
     public $properties = [
         'file_type' => [
-            self::defaultUserGroup => 'pdf',
+            self::DEFAULT_USER_GROUP => 'pdf',
         ],
         'number_of_comments' => [
-            self::defaultUserGroup => 9,
+            self::DEFAULT_USER_GROUP => 9,
         ]
     ];
 }
--- examples/XmlExample.php
+++ examples/XmlExample.php
@@ -1,8 +1,9 @@
 <?php
 
 require_once __DIR__ . '/../vendor/autoload.php';
-require_once __DIR__ . '/ItemProduct.php';
-require_once __DIR__ . '/ItemContent.php';
+require_once __DIR__ . '/ExampleBaseItem.php';
+require_once __DIR__ . '/ExampleProductItem.php';
+require_once __DIR__ . '/ExampleContentItem.php';
 
 use FINDOLOGIC\Export\Data\Attribute;
 use FINDOLOGIC\Export\Data\Image;
@@ -14,8 +15,9 @@ use FINDOLOGIC\Export\Data\Usergroup;
 use FINDOLOGIC\Export\Exporter;
 
 /**
- * This example class builds a xml export based on the example of the FINDOLOGIC documentation, which can be found
- * under the following link https://docs.findologic.com/doku.php?id=export_patterns:xml#example_xml
+ * This example class builds an XML export based on the example of the FINDOLOGIC documentation, which can be found
+ * under the following link:
+ * @link https://docs.findologic.com/doku.php?id=xml_export_documentation:XML_format
  */
 class XmlExample
 {
@@ -29,8 +31,8 @@ class XmlExample
         $items = [];
         $exporter = Exporter::create(Exporter::TYPE_XML);
 
-        $products[] = new ItemProduct();
-        $products[] = new ItemContent();
+        $products[] = new ExampleProductItem();
+        $products[] = new ExampleContentItem();
 
         foreach ($products as $product) {
             $this->item = $exporter->createItem($product->id);
@@ -57,7 +59,7 @@ class XmlExample
         return $exporter->serializeItems($items, 0, 2, 2);
     }
 
-    private function addOrderNumbers($product): void
+    private function addOrderNumbers(ExampleBaseItem $product): void
     {
         foreach ($product->orderNumbers as $userGroup => $orderNumbers) {
             foreach ($orderNumbers as $orderNumber) {
@@ -66,42 +68,42 @@ class XmlExample
         }
     }
 
-    private function addNames($product): void
+    private function addNames(ExampleBaseItem $product): void
     {
         foreach ($product->names as $userGroup => $name) {
             $this->item->addName($name, $userGroup);
         }
     }
 
-    private function addSummaries($product): void
+    private function addSummaries(ExampleBaseItem $product): void
     {
         foreach ($product->summaries as $userGroup => $summary) {
             $this->item->addSummary($summary, $userGroup);
         }
     }
 
-    private function addDescriptions($product): void
+    private function addDescriptions(ExampleBaseItem $product): void
     {
         foreach ($product->descriptions as $userGroup => $description) {
             $this->item->addDescription($description, $userGroup);
         }
     }
 
-    private function addPrices($product): void
+    private function addPrices(ExampleBaseItem $product): void
     {
         foreach ($product->prices as $userGroup => $price) {
             $this->item->addPrice($price, $userGroup);
         }
     }
 
-    private function addUrls($product): void
+    private function addUrls(ExampleBaseItem $product): void
     {
         foreach ($product->urls as $userGroup => $url) {
             $this->item->addUrl($url, $userGroup);
         }
     }
 
-    private function addKeywords($product): void
+    private function addKeywords(ExampleBaseItem $product): void
     {
         foreach ($product->keywords as $userGroup => $keywords) {
             foreach ($keywords as $keyword) {
@@ -110,42 +112,42 @@ class XmlExample
         }
     }
 
-    private function addBonuses($product): void
+    private function addBonuses(ExampleBaseItem $product): void
     {
         foreach ($product->bonuses as $userGroup => $bonus) {
             $this->item->addBonus($bonus, $userGroup);
         }
     }
 
-    private function addSalesFrequencies($product): void
+    private function addSalesFrequencies(ExampleBaseItem $product): void
     {
         foreach ($product->salesFrequencies as $userGroup => $salesFrequency) {
             $this->item->addSalesFrequency($salesFrequency, $userGroup);
         }
     }
 
-    private function addDateAddeds($product): void
+    private function addDateAddeds(ExampleBaseItem $product): void
     {
         foreach ($product->dateAddeds as $userGroup => $dateAdded) {
             $this->item->addDateAdded(new DateTime($dateAdded), $userGroup);
         }
     }
 
-    private function addSorts($product): void
+    private function addSorts(ExampleBaseItem $product): void
     {
         foreach ($product->sorts as $userGroup => $sort) {
             $this->item->addSort($sort, $userGroup);
         }
     }
 
-    private function addUserGroups($product): void
+    private function addUserGroups(ExampleBaseItem $product): void
     {
         foreach ($product->userGroups as $userGroup) {
             $this->item->addUsergroup(new Usergroup($userGroup));
         }
     }
 
-    private function addImages($product): void
+    private function addImages(ExampleBaseItem $product): void
     {
         foreach ($product->images as $userGroup => $images) {
             foreach ($images as $image => $type) {
@@ -154,16 +156,22 @@ class XmlExample
         }
     }
 
-    private function addAttributes($product): void
+    private function addAttributes(ExampleBaseItem $product): void
     {
         foreach ($product->attributes as $attributeName => $attributeValues) {
             $this->item->addAttribute(new Attribute($attributeName, $attributeValues));
         }
     }
 
-    private function addProperties($product): void
+    private function addProperties(ExampleBaseItem $product): void
     {
         foreach ($product->properties as $propertyName => $values) {
+            if ($propertyName === 'variants') {
+                foreach ($values as $userGroup => $value) {
+                    $values[$userGroup] = json_encode($value);
+                }
+            }
+
             $propertyElement = new Property($propertyName, $values);
             $this->item->addProperty($propertyElement);
         }
