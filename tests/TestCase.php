88f28b3d12f8454460a6c8d0ca8419ea9b92e7e8
TobiasGraml11 <tobias.graml@nosto.com>
2023-03-20 15:34:59 +0100

    Revert "Set PHP min requirement to 8.1"
    This reverts commit 3d77903f20f35f651641488f7da1116624fa540a.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,4 +6,12 @@ use PHPUnit\Framework\TestCase as BaseTestCase;
 
 class TestCase extends BaseTestCase
 {
+    public static function assertMatchesRegularExpression(string $pattern, string $string, string $message = ''): void
+    {
+        if (method_exists(BaseTestCase::class, 'assertMatchesRegularExpression')) {
+            parent::assertMatchesRegularExpression($pattern, $string, $message);
+        } else {
+            parent::assertRegExp($pattern, $string, $message);
+        }
+    }
 }
